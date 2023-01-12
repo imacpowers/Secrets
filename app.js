c3713b2aa113e3f11ff7b5331eb21920da91dfc1
Andrew <odoleandrew@yahoo.com>
2023-01-12 22:06:08 +0100

    third commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
@@ -20,6 +21,7 @@ app.use(bodyParser.urlencoded({
 
 app.use(session({
   secret: "Our little secret.",
+  //store: MongoStore.create({mongoUri:"mongodb://illuminada:test1234@cluster0.5esdm3p.mongodb.net/secertsDB"}),
   resave: false,
   saveUninitialized: false
 }));
